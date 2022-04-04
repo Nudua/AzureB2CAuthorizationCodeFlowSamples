f5c89232f469b46a9b9a3c6c36c6ec804ea71ba8
Tor Ramstad <tor.audun@gmail.com>
2022-04-04 20:38:54 +0200

    Initial commit + Add Razor Pages Sample

---
+++ RazorB2CCodeFlowSample/Pages/Privacy.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorB2CCodeFlowSample.Pages;
+
+[Authorize]
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
