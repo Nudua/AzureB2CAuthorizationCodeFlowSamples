594f8fbcce7c6adc5b3c6d5be702bb1d51c43826
Tor Ramstad <tor.audun@gmail.com>
2022-04-04 20:41:41 +0200

    Add MVC Sample

---
+++ MVCB2CCodeFlowSample/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MVCB2CCodeFlowSample.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
